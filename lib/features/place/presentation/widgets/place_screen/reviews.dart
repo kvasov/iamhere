a33134754a886896ad4eb24e1a0a6f5408a625f3
Dmitry Kvasov <kvasov85@mail.ru>
2026-02-03 17:36:03 +0300

    reviews author

--- lib/features/place/presentation/widgets/place_screen/reviews.dart
+++ lib/features/place/presentation/widgets/place_screen/reviews.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:iamhere/features/place/domain/entities/review_model.dart';
 import 'section_widget.dart';
+import 'package:iamhere/core/constants/host.dart';
 
 class PlaceReviews extends StatelessWidget {
   final List<ReviewModel> reviews;
@@ -18,17 +19,49 @@ class PlaceReviews extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ...reviews.map(
-              (review) => Padding(
-                padding: const EdgeInsets.only(bottom: 12),
-                child: Text(
-                  review.text,
-                  style: const TextStyle(fontSize: 16),
-                ),
-              ),
+              (review) => _reviewItem(review),
             ),
           ],
         ),
       ),
     );
   }
+}
+
+Widget _reviewItem(ReviewModel review) {
+  return Container(
+    padding: const EdgeInsets.all(12),
+    margin: const EdgeInsets.only(bottom: 12),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(12),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withValues(alpha: 0.05),
+          blurRadius: 8,
+        ),
+      ],
+    ),
+    child: Column(
+      children: [
+        Text(review.text),
+        const SizedBox(height: 12),
+        Row(children: [
+          ClipOval(
+            child: SizedBox(
+              width: 32,
+              height: 32,
+              child: Image.network(
+                'http://$host/${review.author.photoPath.startsWith('/') ? review.author.photoPath.substring(1) : review.author.photoPath}',
+                fit: BoxFit.cover,
+                errorBuilder: (_, __, ___) => const Icon(Icons.broken_image, size: 32),
+              ),
+            ),
+          ),
+          const SizedBox(width: 12),
+          Text(review.author.name, style: const TextStyle(fontSize: 12),),
+        ],),
+      ],
+    ),
+  );
 }
